e080278dec7b746abe1318d46d451197ae03828b
Timothy Miskelley <miskelleytimothy@gmail.com>
2023-01-20 15:30:00 -0500

    Added King movement generator method

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -3,11 +3,24 @@
 # represents chess king moveset and rules
 class King
   attr_accessor :current_pos
-  attr_reader :symbol, :start_pos
+  attr_reader :symbol
+
+  MOVES = [-1, 0, 1].repeated_permutation(2).to_a
 
   def initialize(symbol, start_pos)
     @symbol = symbol
-    @start_pos = start_pos
-    @current_pos = nil
+    @current_pos = start_pos
+    @potential_moves = generate_moves(@current_pos.coordinate.split(''))
+  end
+
+  # generates all possible moves that the King can make
+  def generate_moves(current_spot)
+    current_spot[0] = current_spot[0].ord
+    current_spot[1] = current_spot[1].to_i
+    MOVES
+      .map { |arr| [current_spot[0] + arr[0], current_spot[1] + arr[1]] }
+      .select { |arr| arr[0].between?(97, 104) && arr[1].between?(1, 8) }
+      .map { |arr| [arr[0].chr, arr[1]] }
+      .reject { |arr| arr.join == @current_pos }
   end
 end
