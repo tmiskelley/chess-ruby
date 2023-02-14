f534a34519c68f47c74efa64d677b4bb4306577c
Timothy Miskelley <miskelleytimothy@gmail.com>
2023-02-14 14:34:40 -0500

    Implemented pawn first move ability

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -19,13 +19,27 @@ class Pawn
   private
 
   def generate_moves(current_spot)
+    moves = []
     current_spot[1] = current_spot[1].to_i
 
     @symbol == "\u265f" ? current_spot[1] += 1 : current_spot[1] -= 1
-    [current_spot]
+    moves.push(current_spot)
+
+    if first_move?(current_spot)
+      double_move = current_spot.dup
+
+      @symbol == "\u265f" ? double_move[1] += 1 : double_move[1] -= 1
+      moves.push(double_move)
+    end
+
+    moves
   end
 
   def update_position
     @potential_moves = generate_moves(@current_pos.split(''))
+  end
+
+  def first_move?(current_spot)
+    @symbol == "\u265f" ? current_spot[1] == 3 : current_spot[1] == 6
   end
 end
