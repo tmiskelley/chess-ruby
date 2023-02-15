a02c5e1a5bf0de714f640b033170bb9bd1201345
Timothy Miskelley <miskelleytimothy@gmail.com>
2023-02-15 13:39:04 -0500

    Updated input processing for pawn movement

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -27,20 +27,38 @@ class GameManager
   private
 
   def create_players
-    white_pieces = {
-      'K' => King.new("\u265A", 'e1'),
-      'P' => Pawn.new("\u265f", 'b2')
-    }
-
-    black_pieces = {
-      'K' => King.new("\u2654", 'e8'),
-      'P' => Pawn.new("\u2659", 'c7')
-    }
-
-    white_player = Player.new('White', white_pieces)
-    black_player = Player.new('Black', black_pieces)
+    white_player = Player.new('White', instantiate_white_pieces)
+    black_player = Player.new('Black', instantiate_black_pieces)
 
     [white_player, black_player]
+  end
+
+  def instantiate_white_pieces
+    {
+      'K' => King.new("\u265A", 'e1'),
+      'a' => Pawn.new("\u265f", 'a2'),
+      'b' => Pawn.new("\u265f", 'b2'),
+      'c' => Pawn.new("\u265f", 'c2'),
+      'd' => Pawn.new("\u265f", 'd2'),
+      'e' => Pawn.new("\u265f", 'e2'),
+      'f' => Pawn.new("\u265f", 'f2'),
+      'g' => Pawn.new("\u265f", 'g2'),
+      'h' => Pawn.new("\u265f", 'h2')
+    }
+  end
+
+  def instantiate_black_pieces
+    {
+      'K' => King.new("\u2654", 'e8'),
+      'a' => Pawn.new("\u2659", 'a7'),
+      'b' => Pawn.new("\u2659", 'b7'),
+      'c' => Pawn.new("\u2659", 'c7'),
+      'd' => Pawn.new("\u2659", 'd7'),
+      'e' => Pawn.new("\u2659", 'e7'),
+      'f' => Pawn.new("\u2659", 'f7'),
+      'g' => Pawn.new("\u2659", 'g7'),
+      'h' => Pawn.new("\u2659", 'h7')
+    }
   end
 
   def place_pieces
@@ -53,15 +71,17 @@ class GameManager
 
   def player_move
     input = validate_input
-    @board.move_piece(@current_player.pieces[input[0]], input[1..])
-    @current_player.move_piece(input[0], input[1..])
+    coordinate = set_coordinate(input).join
+
+    @board.move_piece(@current_player.pieces[input[0]], coordinate)
+    @current_player.move_piece(input[0], coordinate)
   end
 
   def validate_input
     input = gets.chomp
 
     unless invalid_input?(input)
-      coordinate = input[1..].split('')
+      coordinate = set_coordinate(input)
       coordinate[1] = coordinate[1].to_i
       return input if potential_move?(input, coordinate) && not_king?(input, coordinate)
     end
@@ -75,6 +95,10 @@ class GameManager
 
   def potential_move?(input, coordinate)
     @current_player.pieces[input[0]].potential_moves.any? { |arr| arr == coordinate }
+  end
+
+  def set_coordinate(input)
+    input.size == 2 ? input[0..].split('') : input[1..].split('')
   end
 
   def not_king?(input, coordinate)
